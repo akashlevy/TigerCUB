f8b7f200740e3e3e2a5018499de0c3b485a2ebbd
Akash Levy <akashlevy@gmail.com>
2014-11-15 22:13:10 -0500

    Updated views and urls

--- PricoX/PricoX/urls.py
+++ PricoX/PricoX/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', 'courses.views.login_user')
-)
+    url(r'^$', 'courses.views.login_user'),
+    url(r'^course_picker/', 'courses.views.course_picker'))
--- PricoX/courses/views.py
+++ PricoX/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
+from courses.models import tags
 
 def login_user(request):
     if request.POST:
@@ -10,7 +11,14 @@ def login_user(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
+                return render_to_repsonse('course_picker.html')
         else:
-            state = "Your username and/or password were incorrect."
+            return render_to_response('auth.html', {"error" : "Username/password incorrect"})
 
     return render_to_response('auth.html')
+
+def course_picker(request):
+    global tags
+    return render_to_response('course_picker.html', {"tags" : tags})
+            
+            
